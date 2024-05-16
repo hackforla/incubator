ca33bdecddac43dbb0fc1c05fb8c7d99748f5ddd
Jimmy Juarez <jjuarez@boingo.com>
2024-05-15 18:27:15 -0700

    Remove people depot mention

--- terraform-incubator/civic-tech-jobs/dev/main.tf
+++ terraform-incubator/civic-tech-jobs/dev/main.tf
@@ -16,8 +16,3 @@ module "dev" {
   source = "../project"
   container_image  = "035866691871.dkr.ecr.us-west-2.amazonaws.com/ctj-backend-dev:latest"
 }
-
-moved {
-  from = module.ecr.aws_ecr_repository.this
-  to   = module.dev.module.people_depot.module.ecr.aws_ecr_repository.this
-}
