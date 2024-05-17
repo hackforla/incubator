c6069076e51a6488757fec897764af90769c2224
Chelsey Beck <cbeck@mozilla.com>
2024-05-17 15:08:16 -0700

    adding cognito module

---
+++ terraform-modules/cognito/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "user_pool_name" {
+  description = "Name of the Cognito User Pool"
+  type        = string
+}
+
+variable "client_name" {
+  description = "Name of the Cognito User Pool Client"
+  type        = string
+}
