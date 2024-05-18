e3c14e8ea1e77d0c81d400efaa124a0d1421214c
Chelsey Beck <cbeck@mozilla.com>
2024-05-18 12:27:24 -0700

    uncommenting to test and adding arbitrary default values

--- terraform-incubator/people-depot/dev/main.tf
+++ terraform-incubator/people-depot/dev/main.tf
@@ -12,22 +12,24 @@ provider "aws" {
   region = "us-west-2"
 }
 
-# variable "root_db_password" {
-#   type        = string
-#   description = "root database password"
-# }
+variable "root_db_password" {
+  type        = string
+  description = "root database password"
+  default     = "password"
+}
 
-# variable "app_db_password" {
-#   type = string
-# }
+variable "app_db_password" {
+  type    = string
+  default = "password"
+}
 
-# module "dev" {
-#   source = "../project"
+module "dev" {
+  source = "../project"
 
-#   root_db_password = var.root_db_password
-#   app_db_password  = var.app_db_password
-#   container_image  = "035866691871.dkr.ecr.us-west-2.amazonaws.com/people-depot-backend-dev:latest"
-# }
+  root_db_password = var.root_db_password
+  app_db_password  = var.app_db_password
+  container_image  = "035866691871.dkr.ecr.us-west-2.amazonaws.com/people-depot-backend-dev:latest"
+}
 
 module "cognito" {
   source = "../../../terraform-modules/cognito"
