61195a027e86082ba2663579ef852a2ea989ee7c
Chelsey Beck <cbeck@mozilla.com>
2024-05-29 18:26:11 -0700

    removing cognito module

--- terraform-modules/cognito/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "user_pool_id" {
-  description = "The ID of the Cognito User Pool"
-  value       = aws_cognito_user_pool.main.id
-}
-
-output "user_pool_client_id" {
-  description = "The ID of the Cognito User Pool Client"
-  value       = aws_cognito_user_pool_client.main.id
-} 
