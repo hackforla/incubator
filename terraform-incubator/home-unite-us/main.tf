eac5e802d238150e6cb2c2e1b1f53470467bdbe2
tylerthome <tyler_thome@yahoo.com>
2024-06-26 19:02:11 -0700

    add action and oidc integration

--- terraform-incubator/home-unite-us/main.tf
+++ terraform-incubator/home-unite-us/main.tf
@@ -9,3 +9,22 @@ resource "aws_route53_record" "www" {
   ttl     = 300
   records = ["18.223.160.58"]
 }
+
+terraform {
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    region   = "us-west-2"
+    key      = "incubator/home-unite-us/dev.tfstate"
+    bucket   = "hlfa-incubator-terragrunt"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
