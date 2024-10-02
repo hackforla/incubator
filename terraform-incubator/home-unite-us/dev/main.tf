21d56a66cd9462f36e0d3932ccd4d13c1f5d4eed
tylerthome <tyler.thome@outlook.com>
2024-10-02 15:29:54 -0700

    datasource for lb listener, add ecr repo

--- terraform-incubator/home-unite-us/dev/main.tf
+++ terraform-incubator/home-unite-us/dev/main.tf
@@ -1,23 +1,9 @@
-resource "aws_route53_zone" "main" {
-  name = "homeunite.us"
-}
-
-resource "aws_route53_record" "www" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "homeunite.us"
-  type    = "A"
-  ttl     = 300
-  records = ["18.223.160.58"]
-}
-
 terraform {
-
   required_providers {
     aws = {
       source = "hashicorp/aws"
     }
   }
-
   backend "s3" {
     region   = "us-west-2"
     key      = "incubator/home-unite-us/dev.tfstate"
@@ -27,4 +13,38 @@ terraform {
 
 provider "aws" {
   region = "us-west-2"
+}
+
+locals {
+  host_name = "homeunite.us"
+  app_name = "homeuniteus"
+  listener_arn = "arn:aws:elasticloadbalancing:us-west-2:035866691871:listener/app/incubator-prod-lb/7451adf77133ef36/390a225766a4daf3"
+}
+
+resource "aws_route53_zone" "main" {
+  name = local.host_name
+}
+
+resource "aws_route53_record" "root_a_record" {
+  zone_id = aws_route53_zone.main.zone_id
+  name    = local.host_name
+  type    = "A"
+  ttl     = 300
+  records = ["18.223.160.58"]
+}
+
+resource "aws_ecr_repository" "this" {
+    image_tag_mutability = "MUTABLE"
+    name                 = local.app_name
+    tags                 = {
+      "Organization" = "Hack for LA"
+    }
+
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+data "aws_lb_listener" "listener" {
+  arn = local.listener_arn
 }
