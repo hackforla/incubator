a288d908447edbc826dcbf65d570b7a878d4d07f
tylerthome <tyler.thome@outlook.com>
2024-10-02 19:41:47 -0700

    generate certs on aws

--- terraform-incubator/home-unite-us/dev/main.tf
+++ terraform-incubator/home-unite-us/dev/main.tf
@@ -128,7 +128,6 @@ resource "aws_security_group" "homeuniteus" {
   tags = { Name = "ecs_container_instance_${local.app_name}" }
 }
 
-
 resource "aws_ecs_service" "homeuniteus" {
   name                   = "homeuniteus"
   cluster                = "arn:aws:ecs:us-west-2:035866691871:cluster/incubator-prod"
@@ -159,3 +158,38 @@ resource "aws_ecs_service" "homeuniteus" {
   }
 }
 
+resource "aws_acm_certificate" "domain" {
+  domain_name               = local.root_host_name
+  validation_method         = "DNS"
+  subject_alternative_names = ["*.${local.root_host_name}"]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.domain.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.original.zone_id
+}
+
+resource "aws_acm_certificate_validation" "domain" {
+  certificate_arn         = aws_acm_certificate.domain.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource "aws_lb_listener_certificate" "domain" {
+  listener_arn = local.listener_arn
+  certificate_arn = aws_acm_certificate_validation.domain.certificate_arn
+}
