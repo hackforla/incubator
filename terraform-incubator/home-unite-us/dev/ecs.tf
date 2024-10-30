c03291e54317cb743dbf6ed11c02e916e33e6c5f
tylerthome <tyler.thome@outlook.com>
2024-10-29 19:07:13 -0700

    bump image for ui dbg

--- terraform-incubator/home-unite-us/dev/ecs.tf
+++ terraform-incubator/home-unite-us/dev/ecs.tf
@@ -42,7 +42,7 @@ resource "aws_ecs_task_definition" "homeuniteus" {
         }]
         essential = true
         # image     = "035866691871.dkr.ecr.us-west-2.amazonaws.com/homeuniteus:20241017.1"
-        image     = "035866691871.dkr.ecr.us-west-2.amazonaws.com/homeuniteus:20241029.1"
+        image     = "035866691871.dkr.ecr.us-west-2.amazonaws.com/homeuniteus:20241029.2"
         logConfiguration = {
           logDriver = "awslogs"
           options = {
