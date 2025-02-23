7f17c1be401ac28525d6d222fd4508fbe2645344
Alex English <alex.english80@gmail.com>
2025-02-23 09:06:01 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,4 +2,8 @@ module "people-depot" {
     source = "./projects/people-depot"
 }
 
+module "civic-tech-jobs" {
+    source = "./projects/civic-tech-jobs"
+}
+
 data "aws_caller_identity" "current" {}
