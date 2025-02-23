95202c9fa7398edbcfb295bc57a6bb88327f3bee
RSkuma <rsakuma4@gmail.com>
2025-02-22 16:52:55 -0800

    changed backend to fullstack in names, also pulled from main to fix OIDC action issues

--- terraform/projects/civic-tech-jobs/civic-tech-jobs.tf
+++ terraform/projects/civic-tech-jobs/civic-tech-jobs.tf
@@ -3,9 +3,9 @@ locals {
     project_name_civic_tech_jobs = "civic-tech-jobs"
 }
 
-module "civic_tech_jobs_ecr_backend" {
+module "civic_tech_jobs_ecr_fullstack" {
    source  "../../modules/ecr"
-   project_name = "${local.project_name_civic_tech_jobs}-backend"
+   project_name = "${local.project_name_civic_tech_jobs}-fullstack"
 } 
 
 module "civic_tech_jobs_cicd" {
