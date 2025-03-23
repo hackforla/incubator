cdc901e0cf3165358f3a999d4c95aebbeeee6d88
Alex English <alex.english80@gmail.com>
2025-03-23 11:49:41 -0700

    backend dev service

--- terraform/projects/vrms/environment-dev.tf
+++ terraform/projects/vrms/environment-dev.tf
@@ -13,7 +13,6 @@ module "dev_database_url_secret" {
 }
 
 
-
 module "backend_dev_service" {
    source = "../../modules/container"
    project_name = local.project_name
@@ -23,11 +22,26 @@ module "backend_dev_service" {
    container_port = 4000
    container_image = "${module.ecr_backend.repository_url}:dev"
    container_environment = [
-      { "name": "BACKEND_PORT", "value": "4000"}
+      { "name": "BACKEND_PORT", "value": "4000"},
+      { "name": "GMAIL_EMAIL", "value": "vrms.signup@gmail.com"},
+      { "name": "MAILHOG_PORT", "value": "1025"},
+      { "name": "REACT_APP_PROXY", "value": "http://localhost:4000"},
+      { "name": "SLACK_CHANNEL_ID", "value": "D018H4TM94P"},
+      { "name": "SLACK_CLIENT_ID", "value": "1302534787829.1327799404688"},
+      { "name": "SLACK_TEAM_ID", "value": "T018WFQP5QD"}
    ]
    container_environment_secrets = [
       { "name": "CUSTOM_REQUEST_HEADER", "valueFrom": module.custom_request_header_secret.arn},
       { "name": "DATABASE_URL", "valueFrom": module.dev_database_url_secret.arn},
+      { "name": "GMAIL_CLIENT_ID", "valueFrom": module.gmail_client_id_secret.arn},
+      { "name": "GMAIL_REFRESH_TOKEN", "valueFrom": module.gmail_refresh_token_secret.arn},
+      { "name": "GMAIL_SECRET_ID", "valueFrom": module.gmail_secret_id_secret.arn},
+      { "name": "MAILHOG_PASSWORD", "valueFrom": module.mailhog_password_secret.arn},
+      { "name": "MAILHOG_USER", "valueFrom": module.mailhog_user_secret.arn},
+      { "name": "SLACK_BOT_TOKEN", "valueFrom": module.slack_bot_token_secret.arn},
+      { "name": "SLACK_CLIENT_SECRET", "valueFrom": module.slack_client_secret_secret.arn},
+      { "name": "SLACK_OAUTH_TOKEN", "valueFrom": module.slack_oauth_token_secret.arn},
+      { "name": "SLACK_SIGNING_SECRET", "valueFrom": module.slack_signing_secret_secret.arn}
    ]
    
    hostname = module.dev_dns_entry.full_dns_name
