9f578812f492b1ddd76840a1b0dbf3f07b3fce8a
Alex English <alex.english80@gmail.com>
2025-10-17 21:37:54 -0700

    docs for ecr and dns-entry

--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,7 +1,9 @@
 output "arn" {
   value = aws_ecr_repository.this.arn
+  description = "ARN of the created ECR repository"
 }
 
 output "repository_url" {
     value = aws_ecr_repository.this.repository_url
+    description = "URL of the docker repository repo"
 }
