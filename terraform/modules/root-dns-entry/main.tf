f54208f97f9346ecae7f43699bf4fc3a759a6e35
Alex English <alex.english80@gmail.com>
2025-10-17 18:15:24 -0700

    root-dns-entry documentation

--- terraform/modules/root-dns-entry/main.tf
+++ terraform/modules/root-dns-entry/main.tf
@@ -1,9 +1,18 @@
+/**
+ * # root-dns-entry
+ *
+ * Creates a Route 53 DNS entry that points to incubator's main ingress (cloudfront or ALB). 
+ * All services that require web access (frontends or API backends) should use this.
+ * 
+ */
 
+
+#terraform-docs-ignore
 data "aws_route53_zone" "this" {
   zone_id = var.zone_id
 } 
 
-
+#terraform-docs-ignore
 data "aws_lb" "this" {
   arn  = "arn:aws:elasticloadbalancing:us-west-2:035866691871:loadbalancer/app/incubator-prod-lb/7451adf77133ef36"
 }
