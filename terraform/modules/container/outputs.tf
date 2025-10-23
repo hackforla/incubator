1c7f2a0a86fe7b5884a0ca3c4d3a7752d86f6473
Alex English <alex.english80@gmail.com>
2025-10-22 18:14:55 -0700

    125 - terraform docs: container (#132)
    * documentation for container tf module
    
    * terraform-docs: automated updates to Terraform modules README.md
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- terraform/modules/container/outputs.tf
+++ terraform/modules/container/outputs.tf
@@ -1,7 +1,9 @@
 output "task_role_arn" {
+  description = "ARN of the task role that this container uses. Good for setting up permissions like s3 access"
   value = aws_iam_role.instance.arn
 }
 
 output "task_role_name" {
+  description = "IAM role name of the task role that this container uses."
   value = aws_iam_role.instance.name
 }
